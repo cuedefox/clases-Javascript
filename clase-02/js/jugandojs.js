c648ca05a456d37190ad05be48357b859c9c2105
cuede <rodrigovergara2001@gmail.com>
2022-06-09 21:57:36 -0300

    aprendo sobre if, else, booleanos y demas

---
+++ clase-02/js/jugandojs.js
@@ -0,0 +1,46 @@
+let vidaPJ = prompt("¿Cuanta vida tenes?, no mas que 100");
+
+if (vidaPJ == 0) {
+    alert("Te moriste :v");
+}
+else {
+    alert("AhahsahsSSHAHShashaHSHAsHAAAAAAAAAAAAAAAAAAAA sos un vivo barbaaarororororororooooo");
+}
+
+if (vidaPJ == 100) {
+    alert("Encima tenes toda la vida wachiiin");
+}
+else if (vidaPJ == 0) {
+    alert("muerto, re muerto");
+}
+else if (vidaPJ > 100) {
+    alert("Deja de chetear la puta que te pario");
+}
+else if (vidaPJ < 20) {
+    alert("Tiene la vida muy baja manitooo :c");
+}
+else if (vidaPJ < 50) {
+    alert("Tenes la vida por menos de la mitad :O");
+}
+else {
+    alert("todo piolita");
+}
+
+let numero1 = prompt("Pone un numero para ver si es mayor que 666");
+let esMayor = numero1 > 666;
+
+if (esMayor) {
+    alert("Sis es mayor");
+}
+else {
+    alert("Es menor");
+}
+
+let edad = prompt("Pone tu edad")
+
+if (edad >= 18) {
+    alert("Sos mayor de edad")
+}
+else {
+    alert("Sos menor de edad")
+}
