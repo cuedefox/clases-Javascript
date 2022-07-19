ae6bba2c52be1d72276eba09df8943c6317a41a3
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2022-07-19 14:04:28 -0300

    termino clase 13

---
+++ clase-13/js/index.js
@@ -0,0 +1,47 @@
+const btn = document.querySelector("#alerta");
+const btn2 = document.querySelector("#btn2");
+const toast = document.querySelector("#toast");
+btn.onclick = mostrarAlerta;
+btn2.onclick = mostrarAlerta2;
+toast.onclick = mostrarToast;
+
+function mostrarAlerta() {
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Something went wrong!',
+        footer: '<a href="">Why do I have this issue?</a>'
+      })
+}
+function mostrarAlerta2() {
+    Swal.fire({
+        title: 'Do you want to save the changes?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Save',
+        denyButtonText: `Don't save`,
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire('Saved!', '', 'success')
+        } else if (result.isDenied) {
+          Swal.fire('Changes are not saved', '', 'info')
+        }
+      })
+}
+function mostrarToast() {
+    Toastify({
+        text: "Estas Tostado",
+        duration: 1500,
+        destination: "https://wow.com",
+        newWindow: true,
+        close: false,
+        gravity: "bottom", // `top` or `bottom`
+        position: "right", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+        onClick: function(){} // Callback after click
+      }).showToast();
+}
